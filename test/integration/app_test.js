63faa9d71be6a4780840cab1a1f82f5ef8c8ccfb
gla741 <34483933+gla741@users.noreply.github.com>
2019-02-06 16:29:44 +0000

    Delete app_test.js

--- test/integration/app_test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const Code   = require('code');
-const Lab    = require('lab');
-const lab    = exports.lab = Lab.script();
-const Server = require('../../app.js');
-
-lab.experiment('Basic HTTP Tests', () => {
-
-});
